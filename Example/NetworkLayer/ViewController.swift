728ce87ac74a546bb2205d3d3305a25d3ac28b76
Waqas <waqas@dapperapps.com.au>
2018-08-20 02:10:01 +0500

    Initial commit

---
+++ Example/NetworkLayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by developer.muzammal@gmail.com on 08/20/2018.
+//  Copyright (c) 2018 developer.muzammal@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
